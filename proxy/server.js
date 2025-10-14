f1f6b14946c46bce2b2f4c71056a57f6567fd7dc
Thoq <tristanshaw000@gmail.com>
2025-10-14 00:56:23 -0400

    raaah

--- proxy/server.js
+++ proxy/server.js
@@ -16,27 +16,27 @@ const app = express();
 const server = http.createServer();
 
 app.get("/uv/uv.config.js", (req, res) => {
-    res.sendFile(path.join(__dirname, 'uv', 'uv.config.js'));
+  res.sendFile(path.join(__dirname, 'uv', 'uv.config.js'));
 })
 
 app.get('/api/github/commits', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.github.com/repos/GraphiteTechnologies/Marble/commits');
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error fetching commits from GitHub');
-    }
+  try {
+    const response = await axios.get('https://api.github.com/repos/GraphiteTechnologies/Marble/commits');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching commits from GitHub');
+  }
 });
 
-app.get('/api/wallpapers', (req, res) => {
-    const wallpaperDir = path.join(__dirname, '..', 'public', 'wallpapers');
-    fs.readdir(wallpaperDir, (err, files) => {
-        if(err) {
-            res.status(500).send('Unable to scan wallpapers directory');
-            return;
-        }
-        res.json(files);
-    });
+app.get('/api/wallpapers', (_req, res) => {
+  const wallpaperDir = path.join(__dirname, '..', 'public', 'wallpapers');
+  fs.readdir(wallpaperDir, (err, files) => {
+    if (err) {
+      res.status(500).send('Unable to scan wallpapers directory');
+      return;
+    }
+    res.json(files);
+  });
 });
 
 app.use('/uv/', express.static(uvPath));
@@ -44,17 +44,17 @@ app.use('/epoxy/', express.static(epoxyPath));
 app.use('/baremux/', express.static(baremuxPath));
 
 server.on('request', (req, res) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    app(req, res);
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  app(req, res);
 });
 
 server.on('upgrade', (req, socket, head) => {
-    if(req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
-    else socket.end();
+  if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+  else socket.end();
 });
 
 const port = 4000;
 server.listen(port, () => {
-    console.log(`Ultraviolet backend running at http://localhost:${port}`);
+  console.log(`Ultraviolet backend running at http://localhost:${port}`);
 });
